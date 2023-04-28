3813e1d73d5de227d5cc8aead8bb3f18975b68dc
unknown <sudir.319@gmail.com>
2023-04-28 07:45:30 +0530

    Implemented BST

--- src/stack/StackByArray.java
+++ src/stack/StackByArray.java
@@ -69,5 +69,33 @@ public class StackByArray {
 		arr = null;
 		System.out.println("Stack is successfully deleted");
 	}//end of method
+	
+public static void main(String[] args) {
+		
+		
+		System.out.println("Creating a stack of size 5...");
+		StackByArray stack = new StackByArray(5);
+		
+		
+		System.out.println("Pushing 6 values in the Stack...");
+		for(int i=0; i<=5; i++) {
+			stack.push(i*10);
+		}
+		
+		
+		System.out.println("Peeking value from stack");
+		stack.peekOperation();
+	
+		
+		System.out.println("Poping 6 values from the Stack...");
+		for(int i=0; i<=5; i++) {
+			stack.pop();
+		}
+		
+		
+		System.out.println("Deleting the Stack...");
+		stack.deleteStack();
+						
+	}
 
 }//end of class
