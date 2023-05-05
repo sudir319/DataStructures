224fc5c0873d55f523cf994ec2ee4462da901994
unknown <sudir.319@gmail.com>
2023-05-05 08:20:55 +0530

    Modified BST Implementation

--- src/tree/binarySearchTree/BinarySearchTreeByLinkedList.java
+++ src/tree/binarySearchTree/BinarySearchTreeByLinkedList.java
@@ -1,9 +1,7 @@
 package tree.binarySearchTree;
 
-
 import java.util.LinkedList;
 import java.util.Queue;
-
 
 import node.BinaryNode;
 
@@ -39,8 +37,6 @@ public class BinarySearchTreeByLinkedList {
 		}
 	}
 
-	
-
 	// creates a new blank new node
 	public BinaryNode createNewNode(int value) {
 		BinaryNode node = new BinaryNode();
@@ -48,13 +44,12 @@ public class BinarySearchTreeByLinkedList {
 		return node;
 	}
 
-	
 	// Deleting a node from BST
 	public void deleteNodeOfBST(int value) {
 		System.out.println("\n\nDeleting " + value + " from BST...");
-		deleteNodeOfBST(root,value);
+		deleteNodeOfBST(root, value);
 	}
-	
+
 	// Helper Method for delete
 	public BinaryNode deleteNodeOfBST(BinaryNode root, int value) {
 		if (root == null) {
@@ -69,9 +64,12 @@ public class BinarySearchTreeByLinkedList {
 
 			if (root.getLeft() != null && root.getRight() != null) { // if nodeToBeDeleted have both children
 				BinaryNode temp = root;
-				BinaryNode minNodeForRight = minimumElement(temp.getRight());// Finding minimum element from right subtree
-				root.setValue(minNodeForRight.getValue()); // Replacing current node with minimum node from right subtree
-				root.setRight(deleteNodeOfBST(root.getRight(), minNodeForRight.getValue()));  // Deleting minimum node from right now
+				BinaryNode minNodeForRight = minimumElement(temp.getRight());// Finding minimum element from right
+																				// subtree
+				root.setValue(minNodeForRight.getValue()); // Replacing current node with minimum node from right
+															// subtree
+				root.setRight(deleteNodeOfBST(root.getRight(), minNodeForRight.getValue())); // Deleting minimum node
+																								// from right now
 			} else if (root.getLeft() != null) {// if nodeToBeDeleted has only left child
 				root = root.getLeft();
 			} else if (root.getRight() != null) {// if nodeToBeDeleted has only right child
@@ -80,10 +78,8 @@ public class BinarySearchTreeByLinkedList {
 				root = null;
 		}
 		return root;
-	}// end of method
+	}
 
-	
-	
 	// Get minimum element in binary search tree
 	public static BinaryNode minimumElement(BinaryNode root) {
 		if (root.getLeft() == null)
@@ -91,15 +87,13 @@ public class BinarySearchTreeByLinkedList {
 		else {
 			return minimumElement(root.getLeft());
 		}
-	}// end of method
+	}
 
-	
 	// Search a node in BST
 	void searchForValue(int value) {
 		searchForValue(root, value);
 	}
-	
-	
+
 	// Search a node in BST
 	BinaryNode searchForValue(BinaryNode node, int value) {
 		if (node == null) {
@@ -113,7 +107,7 @@ public class BinarySearchTreeByLinkedList {
 		} else {
 			return searchForValue(node.getRight(), value);
 		}
-	}// end of method
+	}
 
 	// Level order traversal of BST
 	void levelOrderTraversal() {
@@ -132,98 +126,91 @@ public class BinarySearchTreeByLinkedList {
 			if (presentNode.getRight() != null)
 				queue.add(presentNode.getRight());
 		}
-	}// end of method
+	}
 
 	// Delete entire BST
 	public void deleteTree() {
 		System.out.println("Deleting entire Tree...");
 		root = null;
 		System.out.println("Tree deleted successfully !");
-	}// end of method
-	
-	
-	
+	}
+
 	void printTreeGraphically() {
 		Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
 		Queue<Integer> level = new LinkedList<Integer>();
-		
+
 		int CurrentLevel = 1;
 		boolean previousLevelWasAllNull = false;
 		queue.add(root);
 		level.add(1);
-		
+
 		System.out.println("\nPrinting Level order traversal of Tree...");
-		if(root == null) {
+		if (root == null) {
 			System.out.println("Tree does not exists !");
 			return;
 		}
-		
+
 		while (!queue.isEmpty()) {
-			if(CurrentLevel == level.peek()) { //if we are in the same level
-				if(queue.peek()==null) {
-					queue.add(null);level.add(CurrentLevel+1);
-				}else {
-					queue.add(queue.peek().getLeft());level.add(CurrentLevel+1);
-					queue.add(queue.peek().getRight());level.add(CurrentLevel+1);
+			if (CurrentLevel == level.peek()) { // if we are in the same level
+				if (queue.peek() == null) {
+					queue.add(null);
+					level.add(CurrentLevel + 1);
+				} else {
+					queue.add(queue.peek().getLeft());
+					level.add(CurrentLevel + 1);
+					queue.add(queue.peek().getRight());
+					level.add(CurrentLevel + 1);
 					previousLevelWasAllNull = false;
 				}
-				System.out.print(queue.remove() + "  ");level.remove();
-			}else { //level has changed
+				System.out.print(queue.remove() + "  ");
+				level.remove();
+			} else { // level has changed
 				System.out.println("\n");
 				CurrentLevel++;
-				if(previousLevelWasAllNull == true) {
+				if (previousLevelWasAllNull == true) {
 					break;
 				}
 				previousLevelWasAllNull = true;
 			}
-		}//end of loop
-	}//end of method
-	
-public static void main(String[] args) {
-		
-		//Constructor
+		} 
+	}
+
+	public static void main(String[] args) {
+
 		BinarySearchTreeByLinkedList tree = new BinarySearchTreeByLinkedList();
-		
-		//Inserting values in BST
+
 		System.out.println("Inserting 10 nodes in Tree");
 		tree.insert(100);
 		tree.insert(80);
 		tree.insert(70);
 		tree.insert(90);
 		tree.insert(50);
-		
-		
-		
+
 		tree.printTreeGraphically();
-		
-		//Searching non-existing value in Tree
+
 		System.out.println("\n\nSearching for value on BST...");
 		tree.searchForValue(80);
-		
-		//Searching existing value in Tree
+
 		System.out.println("\nSearching for value on BST...");
 		tree.searchForValue(60);
 
-		
-		//Deleting Node from Tree
-		tree.deleteNodeOfBST(80); //Node does not exists
-		tree.printTreeGraphically();
-		
-		/*tree.deleteNodeOfBST(57); //Node is having 0 Child
-		tree.printTreeGraphically();
-		
-		tree.deleteNodeOfBST(60); //Node is having 1 Child
-		tree.printTreeGraphically();
-		
-		tree.deleteNodeOfBST(50); //Node is having 2 Child
+		tree.deleteNodeOfBST(80);
 		tree.printTreeGraphically();
 
-		//Deleting entire Tree
-		tree.deleteTree();
-		
-		//Traversing Tree
-		tree.levelOrderTraversal();
+		/*
+		 * tree.deleteNodeOfBST(57); //Node is having 0 Child
+		 * tree.printTreeGraphically();
+		 * 
+		 * tree.deleteNodeOfBST(60); //Node is having 1 Child
+		 * tree.printTreeGraphically();
+		 * 
+		 * tree.deleteNodeOfBST(50); //Node is having 2 Child
+		 * tree.printTreeGraphically();
+		 * 
+		 * //Deleting entire Tree tree.deleteTree();
+		 * 
+		 * //Traversing Tree tree.levelOrderTraversal();
 		 */
-	}//end of method
-	
+	}
+
 }// end of class
